61d7827bb4bf5c16da7f8c3e08704e182e5cac01
Greg Charles <gcharles@paypal.com>
2014-09-15 11:39:32 -0700

    Add S17 and S18.

---
+++ s17.rb
@@ -0,0 +1,17 @@
+# P17 (*) Split a list into two parts.
+#     The length of the first part is given.  Use a Tuple for your result.
+#
+#     Example:
+#     scala> split(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+#     res0: (List[Symbol], List[Symbol]) = (List('a, 'b, 'c),List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+class Array
+  def split(n)
+    a = self.each_with_index.select {|el| el.last < n}.map(&:first)
+    return a, self - a
+  end
+end
+
+a, b = %w(a b c d e f g h i j k).split(3)
+p a
+p b
+
---
+++ s18.rb
@@ -0,0 +1,15 @@
+class Array
+  # P18 (**) Extract a slice from a list.
+  #     Given two indices, I and K, the slice is the list containing the elements
+  #     from and including the Ith element up to but not including the Kth
+  #     element of the original list.  Start counting the elements with 0.
+  #
+  #     Example:
+  #     scala> slice(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+  #     res0: List[Symbol] = List('d, 'e, 'f, 'g)
+  def _slice(i,k)
+    self.each_with_index.select {|el| el.last >= i && el.last < k}.map(&:first)
+  end
+end
+
+p %w(a b c d e f g h i j k)._slice(3,7)
