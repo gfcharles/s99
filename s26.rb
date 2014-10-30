cea282f5b80f8845db3e5b357c8880fedc709558
Greg Charles <gcharles@paypal.com>
2014-10-29 18:02:44 -0700

    Changes to s26

--- s26.rb
+++ s26.rb
@@ -9,15 +9,6 @@ Generate the combinations of K distinct objects chosen from the N elements of a 
     scala> combinations(3, List('a, 'b, 'c, 'd, 'e, 'f))
     res0: List[List[Symbol]] = List(List('a, 'b, 'c), List('a, 'b, 'd), List('a, 'b, 'e), ...
 =end
-class Enumerator
-  def +(addend)
-    Enumerator.new do |y|
-      self.each {|el| y << el}
-      addend.each {|el| y << el}
-    end
-  end
-end
-
 class Array
   def _combination(n)
     return [[]] if n == 0
@@ -33,3 +24,4 @@ p (?a..?f).to_a._combination(3).to_a
 
 p (1..5).to_a._combination(3).to_a
 p (1..5).to_a._combination(3).map {|combo| combo.inject &:+}
+
