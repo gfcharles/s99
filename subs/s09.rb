60ba393b48a7c454eddd55d06eae612cb3322916
Greg Charles <gregcharles@javaranch.com>
2019-08-09 14:21:28 -0700

    Add subdir

---
+++ subs/s09.rb
@@ -0,0 +1,19 @@
+class Array
+  def pack
+    self.reduce([]) do |z, x|
+      case
+        when z.empty?
+          z << [x]
+        when z.last.first == x
+          z.last << x
+        else
+          z << [x]
+      end
+      z
+    end
+  end
+end
+
+# p [?a, ?a, ?a, ?a, ?b, ?c, ?c, ?a, ?a, ?d, ?e, ?e, ?e, ?e].pack
+# p [1].pack
+# p [].pack
